a3f734b512afd191bd37b8de9b9fd6c55e7c30c5
marshall5368 <136056077+marshall5368@users.noreply.github.com>
2024-04-04 15:39:46 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const photoSrc = ['src/shoes src/shoes_2-removebg-preview.png','src/shoes src/shoes_3-removebg-preview.png','src/shoes src/shoes_4-removebg-preview.png','src/shoes src/shoes_5-removebg-preview.png','src/shoes src/shoes_6-removebg-preview.png','src/shoes src/shoes_7-removebg-preview.png','src/shoes src/shoes_9-removebg-preview.png','src/shoes src/shoes_10-removebg-preview.png']
+var display = document.getElementById('imageHome')
+var photoCount = 1
+
+function arrowNext(){
+    display.style.opacity = "0"
+    display.style.translate="200px"
+    setTimeout(function(){
+         photoCount = (photoCount + 1)% photoSrc.length;
+    display.src = photoSrc[photoCount]
+        display.style.opacity="1"
+        display.style.translate = "0"
+    },400)
+   
+    
+}
+function arrowBack(){
+
+    display.style.opacity = "0"
+    display.style.translate="-200px"
+    setTimeout(function(){
+         photoCount = (photoCount - 1)% photoSrc.length;
+    display.src = photoSrc[photoCount]
+        display.style.opacity="1"
+        display.style.translate = "0"
+
+        if(photoCount < 1){
+            photoCount = 8
+        }else{
+          console.log("error")
+        }
+    },400)
+     
+}
